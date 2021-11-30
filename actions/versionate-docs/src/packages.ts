928bf6289906b058dd8e15415eee544975953150
Fanil Zubairov <fannisco@gmail.com>
2021-11-30 21:25:25 +0200

    ci: Improve VD workflow

---
+++ actions/versionate-docs/src/packages.ts
@@ -0,0 +1,21 @@
+export const associatedDocs: Record<string, string> = {
+    "plasma-ui": "plasma-ui-docs",
+    "plasma-web": "plasma-web-docs",
+    "plasma-b2c": "plasma-web-docs",
+    "plasma-temple": "plasma-temple-docs",
+    "plasma-ui-docs": "plasma-ui-docs",
+    "plasma-web-docs": "plasma-web-docs",
+    "plasma-temple-docs": "plasma-temple-docs",
+};
+
+export const associatedPackages: Record<string, string> = {
+    "plasma-ui-docs": "plasma-ui",
+    "plasma-web-docs": "plasma-web",
+    "plasma-temple-docs": "plasma-temple",
+};
+
+export const associatedPrefixes: Record<string, string> = {
+    "plasma-ui-docs": "ui",
+    "plasma-web-docs": "web",
+    "plasma-temple-docs": "temple",
+};
